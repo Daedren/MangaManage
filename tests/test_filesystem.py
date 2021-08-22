f393aecdd65886b287d09c1495939b7d44593701
Daedren <6081828+Daedren@users.noreply.github.com>
2021-08-22 15:45:29 +0100

    Testing quarantine edgecases further

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -7,8 +7,11 @@ from manga.gateways.filesystem import FilesystemGateway
 class TestFilesystemGateway(unittest.TestCase):
     def setUp(self) -> None:
         '''Creates two series with one chapter each for basic tests'''
-        self.sut = FilesystemGateway(sourceFolder='', archiveFolder='', quarantineFolder='')
+        self.sut = FilesystemGateway(sourceFolder='', archiveFolder='/tmp/fstest', quarantineFolder='/tmp/fsquarantine')
+        self.chapterOne = "chapterone"
+
         self.series1 = "/tmp/fstest/seriesone"
+        self.series1Quarantine = "/tmp/fsquarantine/seriesone"
         self.series1Path = Path(self.series1)
         self.series1chapter1 = "/tmp/fstest/seriesone/chapterone"
         self.series1chapter1Path = Path(self.series1chapter1)
@@ -28,6 +31,7 @@ class TestFilesystemGateway(unittest.TestCase):
 
     def tearDown(self) -> None:
         shutil.rmtree('/tmp/fstest/')
+        shutil.rmtree('/tmp/fsquarantine/')
         return super().tearDown()
     
     def test_deleteFolder_onechapter_deletesseries(self):
@@ -65,3 +69,23 @@ class TestFilesystemGateway(unittest.TestCase):
         chapter = '54'
         anilistId = 98563
         self.sut.deleteArchive(anilistId, chapter)
+
+    def test_quarantine_normal_move(self):
+        series1quarantinePath = Path(self.series1Quarantine)
+        series1quarantineChPath = series1quarantinePath.joinpath(self.chapterOne)
+        self.sut.quarantineSeries('seriesone')
+        self.assertTrue(series1quarantinePath.exists())
+        self.assertTrue(series1quarantineChPath.exists())
+        self.assertFalse(self.series1Path.exists())
+
+    def test_quarantine_mixed_merge(self):
+        '''In case the series was already quarantined
+        don't delete existing qt chapters when doing it for other chapters'''
+        series1quarantinePath = Path(self.series1Quarantine)
+        series1quarantineChPath = series1quarantinePath.joinpath(self.chapterOne)
+        series1quarantineChOtherPath = series1quarantinePath.joinpath("alreadyThere")
+        series1quarantineChOtherPath.mkdir(parents=True, exist_ok=False)
+
+        self.sut.quarantineSeries('seriesone')
+        self.assertTrue(series1quarantineChOtherPath.exists())
+        self.assertTrue(series1quarantineChPath.exists())
