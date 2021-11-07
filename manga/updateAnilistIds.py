39721e020a7d8edc9328cb2925c82b9b1126ac8e
Daedren <6081828+Daedren@users.noreply.github.com>
2021-11-07 14:14:59 +0000

    A good match was chosen immediately rather than after checking every option

--- manga/updateAnilistIds.py
+++ manga/updateAnilistIds.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from typing import Optional, Tuple
 from cross.decorators import Logger
 from manga.gateways.database import DatabaseGateway
@@ -9,49 +10,53 @@ from .utils.pylev import levenschtein
 @Logger
 class UpdateTrackerIds:
     """Updates local DB with tracker's IDs"""
-
+    
     def __init__(self, database: DatabaseGateway, anilist: AnilistGateway) -> None:
         self.anilist = anilist
         self.database = database
 
-    def __tryTuple(
-        self, entry: TrackerSeries, series: str, force=False, interactive=False
-    ) -> Optional[Tuple[str, str]]:
+    class FoundEntry:
+        "Model to hold find results"
+        def __init__(self, series_name: str, tracker_entry: TrackerSeries):
+            self.series_name = series_name
+            self.tracker_entry = tracker_entry
+
+
+    def __findTrackerForSeries(
+        self, tracker_entries: [TrackerSeries], series: str, force=False, interactive=False
+    ) -> FoundEntry:
         series_to_match = series.lower()
-        bestMatch = ""
+        bestMatch: Optional[TrackerSeries] = None
         bestMatchDistance = 999
+        
+        for tracker_entry in tracker_entries:
+            for tracker_title in tracker_entry.titles:
+                prepped_title = tracker_title.lower()
+                rdistance = levenschtein(series_to_match, prepped_title)
+                if rdistance < bestMatchDistance:
+                    bestMatch = tracker_entry
+                    bestMatchDistance = rdistance
+                if rdistance < 10:
+                    self.logger.info(
+                        (f"possible match <{prepped_title}>"
+                         f" - dist {rdistance} | id [{tracker_entry.tracker_id}]")
+                    )
 
-        for tracker_title in entry.titles:
-            prepped_title = tracker_title.lower()
-            rdistance = levenschtein(series_to_match, prepped_title)
-            if rdistance < bestMatchDistance:
-                bestMatch = prepped_title
-                bestMatchDistance = rdistance
-
-        if force:
-            return (series, entry.tracker_id)
-
-        if bestMatchDistance <= 4:
-            self.logger.info(f'found "{bestMatch}"')
-            return (series, entry.tracker_id)
-        elif bestMatchDistance < 10:
-            self.logger.info(
-                (f"possible match at {bestMatchDistance}",
-                 f" - {bestMatch} [{entry.tracker_id}]")
-            )
-            if interactive:
-                userValue = input("Enter Anilist ID: ")
-                return (series, userValue)
+        if interactive:
+            self.logger.info(f"Best match at distance {bestMatchDistance} | {bestMatch.tracker_id}")
+            userValue = input("Enter Anilist ID: ")
+            return self.FoundEntry(series, userValue)
+        elif bestMatchDistance < 4:
+            return self.FoundEntry(series, bestMatch.tracker_id)
         return None
 
     def updateFor(self, series, interactive=False) -> Optional[str]:
         self.logger.info("Updating for " + series)
         entries = self.anilist.getAllEntries()
-        for entry in entries.values():
-            result = self.__tryTuple(entry, series, interactive=interactive)
-            if result is not None:
-                self.database.insertTracking(result[0], result[1])
-                return result[1]
+        result = self.__findTrackerForSeries(entries.values(), series, interactive=interactive)
+        if result is not None:
+            self.database.insertTracking(result.series_name, result.tracker_entry)
+            return result.tracker_entry
 
     def manualUpdateFor(self, series, anilistId):
         self.database.insertTracking(series, anilistId)
@@ -64,11 +69,10 @@ class UpdateTrackerIds:
 
         toadd = list()
 
-        for entry in entries:
-            for row in rows:
-                result = self.__tryTuple(entry, row["chapter"])
-                if result is not None:
-                    toadd.append(result)
+        for row in rows:
+            result = self.__findTrackerForSeries(entries, row["series"])
+            if result is not None:
+                toadd.append(result)
 
         for newtuple in toadd:
-            self.database.insertTracking(seriesName=newtuple[0], anilistId=newtuple[1])
+            self.database.insertTracking(seriesName=newtuple.series_name, anilistId=newtuple.tracker_entry)
